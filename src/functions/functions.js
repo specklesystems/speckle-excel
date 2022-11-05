a7425c400bd96d0075e1eb0bc123e926dabef240
Connor Ivy <connor@speckle.systems>
2022-11-04 19:06:53 -0500

    it builds!

---
+++ src/functions/functions.js
@@ -0,0 +1,11 @@
+/**
+ * Add two numbers
+ * @customfunction
+ * @param {number} first First number
+ * @param {number} second Second number
+ * @returns {number} The sum of the two numbers.
+ */
+function add(first, second) {
+  return first + second
+}
+add(1, 2)
